ed5a62502e6c15f45801c3c5d93e54ce4337412b
Vasudev <Vasudev@codewithvasudev.com>
2023-08-20 12:34:25 +0530

    REACT

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item</h2>
+      <div>
+        <h2>Food</h2>
+        <div>Rs 10</div>
+        <h2>Petrol</h2>
+        <div>Rs 100</div>
+        <h2>Movies</h2>
+        <div>Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
