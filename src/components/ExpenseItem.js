379132ac787277df57fd39d947e34908e184c8e8
Vasudev <Vasudev@codewithvasudev.com>
2023-08-21 13:55:47 +0530

    Passing Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,45 @@
-function ExpenseItem() {
-  return (
-    <div>
-      <h2>Expense Item</h2>
-      <div>
-        <h2>Food</h2>
-        <div>Rs 10</div>
-        <h2>Petrol</h2>
-        <div>Rs 100</div>
-        <h2>Movies</h2>
-        <div>Rs 200</div>
+import "./ExpenseItem.css";
+
+// function ExpenseItem() {
+//   const LocationOfExpenditure = ["Food","Petrol","Movies"]; 
+
+//   return (
+//     <div>
+//       <div className="expense-item">
+//         <h2>Food</h2>
+//         <div className="expense-item_price">10</div>
+//         <div className="expense-item_location">{LocationOfExpenditure[0]}</div>
+//       </div>
+//       <div className="expense-item">
+//         <h2>Petrol</h2>
+//         <div className="expense-item_price">100</div>
+//         <div className="expense-item_location">{LocationOfExpenditure[1]}</div>
+//       </div>
+//       <div className="expense-item">
+//         <h2>Movies</h2>
+//         <div className="expense-item_price">200</div>
+//         <div className="expense-item_location">{LocationOfExpenditure[2]}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+////*Passing Data*////
+
+function ExpenseItem(props){
+
+  return(
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item_description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item_price'>${props.amount}</div>
+        <div>{props.locationofExpenditure}</div>
       </div>
     </div>
-  );
+  )
 }
 
 export default ExpenseItem;
