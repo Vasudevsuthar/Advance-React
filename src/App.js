4dd85bb3626dad9ab9ee79aafa17d22d698390ff
Vasudev <Vasudev@codewithvasudev.com>
2023-08-23 12:19:42 +0530

    Task 12

--- src/App.js
+++ src/App.js
@@ -1,134 +1,36 @@
-import ExpenseDetails from "./components/ExpenseDetails";
-// import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import Expenses from "./components/Expenses/Expenses";
 
-  // //*Task 9*//
-  // const expenses = [
-  //   {
-  //     id: "e1",
-  //     title: "Toilet Paper",
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: "e3",
-  //     title: "Car Insurance",
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     id: "e4",
-  //     title: "New Desk (Wooden)",
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   },
-  // ];
-
-  // return (
-  //   <div>
-  //     <h2>Let's get started</h2>
-  //     {expenses.map((expenses) => (
-  //       <ExpenseItem
-  //         key={expenses.id}
-  //         title={expenses.title}
-  //         amount={expenses.amount}
-  //         date={expenses.date}
-  //         locationofExpenditure={expenses.title}
-  //       />
-  //     ))}
-  //   </div>
-  // );
-
-
-
-// Task 10 First part*//
-
-  // const expenses = [
-  //  {
-  //   id: "e1",
-  //   title: "Toilet Paper",
-  //   amount: 94.12,
-  //   date: new Date(2020, 7, 14),
-  //  },
-  //  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  //  {
-  //   id: "e3",
-  //   title: "Car Insurance",
-  //   amount: 294.67,
-  //   date: new Date(2021, 2, 28),
-  //  },
-  //  {
-  //   id: "e4",
-  //   title: "New Desk (Wooden)",
-  //   amount: 450,
-  //   date: new Date(2021, 5, 12),
-  //  },
-  // ];
+const App = () => {
  
-  // return (
-  //  <div>
-  //   <h2>Let's get started</h2>
-  //   <ExpenseItem
-  //    title={expenses[0].title}
-  //    amount={expenses[0].amount}
-  //    date={expenses[0].date}
-  //    locationofExpenditure={expenses.title}
-  //   ></ExpenseItem>
-  //   <ExpenseItem
-  //    title={expenses[1].title}
-  //    amount={expenses[1].amount}
-  //    date={expenses[1].date}
-  //    locationofExpenditure={expenses.title}
-  //   ></ExpenseItem>
-  //   <ExpenseItem
-  //    title={expenses[2].title}
-  //    amount={expenses[2].amount}
-  //    date={expenses[2].date}
-  //    locationofExpenditure={expenses.title}
-  //   ></ExpenseItem>
-  //   <ExpenseItem
-  //    title={expenses[3].title}
-  //    amount={expenses[3].amount}
-  //    date={expenses[3].date}
-  //    locationofExpenditure={expenses.title}
-  //   ></ExpenseItem>
-  //  </div>
-  // );
-
-
-  //*Task 10 second part*//
-
   const expenses = [
-    {
-     id: "e1",
-     title: "Toilet Paper",
-     amount: 94.12,
-     date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-     id: "e3",
-     title: "Car Insurance",
-     amount: 294.67,
-     date: new Date(2021, 2, 28),
-    },
-    {
-     id: "e4",
-     title: "New Desk (Wooden)",
-     amount: 450,
-     date: new Date(2021, 5, 12),
-    },
-   ];
-  
-   return (
-    <div>
-     <h2>Let's get started</h2>
-     {expenses.map((expense,index) => (<ExpenseDetails key={index} title={expense.title} amount={expense.amount}/>
-     ))}
-    </div>
-   );
-  }
+   {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+   },
+   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+   {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+   },
+   {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+   },
+  ];
+ 
+  return (
+   <div>
+    <h2>Let's get started</h2>
+    <Expenses items={expenses}/>
+   </div>
+  );
+}
  
 
 export default App;
