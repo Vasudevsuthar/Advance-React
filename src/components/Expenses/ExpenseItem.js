2d18434739b7839e7ef842ef8ca3f77d0748d73e
Vasudev <Vasudev@codewithvasudev.com>
2023-08-24 22:05:13 +0530

    button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,20 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
-
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!!!')
+  };
+
+  const deleteHandler = () => {
+    const ExpenseItem = document.querySelector(".expense-item");
+    if(ExpenseItem){
+      ExpenseItem.remove();
+    }
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +22,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete ExpenseItem</button>
     </Card>
   );
 }
