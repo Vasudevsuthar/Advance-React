09764b6c88da6cd3e040b6fdfc9565c3c41f3e75
Vasudev <Vasudev@codewithvasudev.com>
2023-08-31 10:39:59 +0530

    Task 12

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpenseList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -11,21 +12,9 @@ const Expenses = (props) => {
     selFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expenseContent = <p>No expenses found</p>
-
-  if(filteredExpenses.length > 0){
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <div>
@@ -34,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
